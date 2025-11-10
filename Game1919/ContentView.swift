1593dad302badb04cb179c43b343acfcb2e38abb
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-11-10 16:51:30 +0100

    SLIDER

--- Game1919/ContentView.swift
+++ Game1919/ContentView.swift
@@ -9,25 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var alertVisible = false
-    
+    @State var sliderValue = 50.0
     var body: some View {
         VStack(spacing: 5) {
+            Color("BackgroundColor").ignoresSafeArea()
+            //VStack()
             Text("🙈🙉🙊")
                 .font(.largeTitle)
             Text("80")
-            HStack {
-                Text("1").fontWeight(.bold)
-                Slider(value: .constant(50), in: 1...100)
-                Text("100").fontWeight(.bold)
-            }
-            .padding(.horizontal)
-            
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .kerning(-1)
+            SliderView(value: $sliderValue, lowValue: 1,highValue: 100)
+            Text("Slider value tracking \(sliderValue)")
             Button("TRY") {
                 alertVisible = true
             }
             .padding()
             .font(.title)
-            .background(Color.blue)
+            .background(Color.red)
             .foregroundColor(.white)
             .cornerRadius(21.0)
             .alert(isPresented: $alertVisible) {
@@ -44,5 +44,19 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SliderView: View {
+    @Binding var value:Double
+    let lowValue:Double
+    let highValue:Double
+    var body: some View {
+        HStack {
+            Text("\(Int(lowValue))").fontWeight(.bold)
+            Slider(value: $value, in: lowValue...highValue)
+            Text("\(Int(highValue))").fontWeight(.bold)
+        }
+        .padding(.horizontal)
     }
 }
