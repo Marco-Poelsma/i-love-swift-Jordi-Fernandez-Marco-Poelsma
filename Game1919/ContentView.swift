0d7bf35eb2f493cba40ec870662a79692c774970
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-11-10 16:21:54 +0100

    Initial Commit

---
+++ Game1919/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Game
+//
+//  Created by alumne on 27/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var alertVisible = false
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            Text("---")
+                .font(.largeTitle)
+            Text("80")
+            HStack {
+                Text("1").fontWeight(.bold)
+                Slider(value: .constant(50), in: 1...100)
+                Text("100").fontWeight(.bold)
+            }
+            .padding(.horizontal)
+            
+            Button("TRY") {
+                alertVisible = true
+            }
+            .padding()
+            .font(.title)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(21.0)
+            .alert(isPresented: $alertVisible) {
+                Alert(
+                    title: Text("Hello"),
+                    message: Text("This is my first alert"),
+                    dismissButton: .default(Text("Got it"))
+                )
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game1919/Game1919App.swift
@@ -0,0 +1,17 @@
+//
+//  Game1919App.swift
+//  Game1919
+//
+//  Created by alumne on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Game1919App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
